1ce39a83d6f61409a084a51bd99820ed9dd9fd5a
dimastr90 <34780847+dimastr90@users.noreply.github.com>
2020-01-19 18:36:54 +0200

    Create reverseInParentheses.js

---
+++ Intro/reverseInParentheses.js
@@ -0,0 +1,40 @@
+/* Write a function that reverses characters in (possibly nested) parentheses in the input string.
+
+Input strings will always be well-formed with matching ()s.
+
+Example
+
+For inputString = "(bar)", the output should be
+reverseInParentheses(inputString) = "rab";
+For inputString = "foo(bar)baz", the output should be
+reverseInParentheses(inputString) = "foorabbaz";
+For inputString = "foo(bar)baz(blim)", the output should be
+reverseInParentheses(inputString) = "foorabbazmilb";
+For inputString = "foo(bar(baz))blim", the output should be
+reverseInParentheses(inputString) = "foobazrabblim".
+Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string inputString
+
+A string consisting of lowercase English letters and the characters ( and ). It is guaranteed that all parentheses in inputString form a regular bracket sequence.
+
+Guaranteed constraints:
+0 ≤ inputString.length ≤ 50.
+
+[output] string
+
+Return inputString, with all the characters that were in parentheses reversed.
+*/
+
+function reverseInParentheses(inputString) {
+    var re = /(\(([a-z]+)?\))/; 
+    while(inputString.search(re)!==-1){
+        res = inputString.match(re)[0].split("").reverse().join("");;
+        inputString = inputString.replace(re,res.substring(1,res.length-1));
+    }
+    
+    return inputString;
+}
